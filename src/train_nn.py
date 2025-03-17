29aa2b6377193304d042a2a3db7b7dd1ea6e8db0
grnimrod <nimrod.grandpierre@gmail.com>
2025-03-17 13:26:19 +0100

    Deleted files

--- src/train_nn.py
+++
@@ -1,6 +0,0 @@
-from dataset import CustomDataset
-from splits import create_splits
-
-dataset = CustomDataset("./../data/15mer_A.tsv")
-
-train_dataset, val_dataset, test_dataset = create_splits(dataset)
