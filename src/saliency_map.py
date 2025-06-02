533b00135c73ebd1d63c1241b5107a0d1b5ed080
grnimrod <nimrod.grandpierre@gmail.com>
2025-06-02 14:27:19 +0200

    feat: Refine saliency map figures

--- src/saliency_map.py
+++ src/saliency_map.py
@@ -1,7 +1,9 @@
 import torch
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
+import colorcet as cc
 
 from utils.load_splits import load_splits
 from model_definitions import ModularModel
@@ -23,7 +25,8 @@ def obtain_saliency_map(data_version):
     avg_saliency_per_position = []
     avg_saliency_matrix = []
 
-    for index in range(len(X_local_test)):
+    # for index in range(len(X_local_test)):
+    for index in range(100):
         X = X_local_test[index].unsqueeze(0).clone().detach()
         
         X.requires_grad_()
@@ -50,14 +53,37 @@ def obtain_saliency_map(data_version):
     avg_saliency_per_position = np.mean(avg_saliency_per_position, axis=0)
 
     avg_saliency_matrix = np.mean(avg_saliency_matrix, axis=0)
-    print(f"Avg of saliency matrix: {avg_saliency_matrix}")
 
+    plt.rcParams.update({"font.family": "monospace"})
+
+    # Saliency map as bar chart
     plt.figure(figsize=(12, 2))
-    plt.bar(x=range(len(avg_saliency_per_position)), height=avg_saliency_per_position)
+    plt.bar(
+        x=range(1, len(avg_saliency_per_position) + 1),
+        height=avg_saliency_per_position,
+        color=mpl.colormaps['cet_glasbey_dark'].colors[3]
+    )
+    plt.xlabel("Position", fontweight="bold")
+    plt.gca().set_xticks(range(1, len(avg_saliency_per_position) + 1))
+    plt.gca().set_xticklabels(range(1, len(avg_saliency_per_position) + 1))
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+    plt.gca().grid(axis="y", linestyle="--", alpha=0.6, zorder=0)
     plt.tight_layout()
     plt.show()
 
+    # Averaged saliency map as heatmap
+    plt.figure(figsize=(len(avg_saliency_per_position), 1))
+    sns.heatmap(np.expand_dims(avg_saliency_per_position, 1).T, cmap="hot")
+    plt.gca().tick_params(left=False, bottom=False)
+    plt.gca().set(xticklabels=[], yticklabels=[])
+    # plt.xlabel("Position", fontweight="bold")
+    plt.tight_layout()
+    plt.show()
+
+    # Nucleotide-specific saliency map as heatmap
     plt.figure(figsize=(12, 2))
-    sns.heatmap(avg_saliency_matrix.T, cmap="hot", yticklabels=["A", "C", "G", "T"])
+    sns.heatmap(avg_saliency_matrix.T, cmap="hot", xticklabels=range(1, avg_saliency_matrix.shape[0] + 1), yticklabels=["A", "C", "G", "T"])
+    plt.xlabel("Position", fontweight="bold")
     plt.tight_layout()
     plt.show()
