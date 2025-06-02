3485371571e872f1a43591db65818ae5690a3fa7
grnimrod <nimrod.grandpierre@gmail.com>
2025-06-02 10:39:24 +0200

    feat: Add support for creating and saving outcome-specific avg mut feature

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,8 +1,8 @@
 import os
 import re
-from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 import argparse
 
 
@@ -46,11 +46,31 @@ def preprocess_data(tsv_data, train_ratio=0.7, random_state=42):
     train_dataset, temp_dataset = train_test_split(df, train_size=train_ratio, random_state=random_state, stratify=df["mut"])
     val_dataset, test_dataset = train_test_split(temp_dataset, test_size=0.5, random_state=random_state, stratify=temp_dataset["mut"])
 
-    # Compute average mutation rate per bin feature separately for the splits
-    for label, size in zip(bins, bin_sizes):
-        train_dataset[f"avg_mut_{label}"] = train_dataset.groupby(f"bin_id_{label}")["mut"].transform("mean")
-        val_dataset[f"avg_mut_{label}"] = val_dataset.groupby(f"bin_id_{label}")["mut"].transform("mean")
-        test_dataset[f"avg_mut_{label}"] = test_dataset.groupby(f"bin_id_{label}")["mut"].transform("mean")
+    # Add both overall and outcome-specific average mutation rate per bin columns
+    def add_avg_mut_per_bin_columns(df):
+        for label in bins:
+            # Add overall average mutation rate
+            df[f"avg_mut_{label}"] = df.groupby(f"bin_id_{label}")["mut"].transform("mean")
+
+            # Count all rows per group
+            group_counts = df.groupby(f"bin_id_{label}").size()
+            for base in ["A", "C", "G", "T"]:
+                # Filter down data to specific mutation outcome
+                df_filtered = df[df["mut_res"] == base]
+
+                # Count filtered rows per group
+                filtered_group_counts = df_filtered.groupby(f"bin_id_{label}").size()
+
+                # Obtain ratio of specific outcome to all outcomes
+                type_spec_mut_ratio_per_bin = (filtered_group_counts / group_counts).fillna(0.0)
+
+                # Map returned Series on original dataframe, add this as new column
+                df[f"avg_mut_{label}_{base}"] = df[f"bin_id_{label}"].map(type_spec_mut_ratio_per_bin)
+        return df
+
+    train_dataset = add_avg_mut_per_bin_columns(train_dataset)
+    val_dataset = add_avg_mut_per_bin_columns(val_dataset)
+    test_dataset = add_avg_mut_per_bin_columns(test_dataset)
     
     def one_hot_encode(dataframe):
         # Convert arrays to 2D arrays of characters ("ACA" -> ["A", "C", "A"])
@@ -79,18 +99,23 @@ def preprocess_data(tsv_data, train_ratio=0.7, random_state=42):
     splitnames = ["X_local_train", "y_train", "X_local_val", "y_val", "X_local_test", "y_test"]
     splits = [X_local_train, y_train, X_local_val, y_val, X_local_test, y_test]
 
-    for bin_size in bins:
+    for label in bins:
         for split_name, dataset in zip(["train", "val", "test"], [train_dataset, val_dataset, test_dataset]):
 
             # Add bin id columns to splits to be saved
-            split_bin_id = f"X_bin_id_{bin_size}_{split_name}"
+            split_bin_id = f"X_bin_id_{label}_{split_name}"
             splitnames.append(split_bin_id)
-            splits.append(dataset[f"bin_id_{bin_size}"].to_numpy())
+            splits.append(dataset[f"bin_id_{label}"].to_numpy().reshape(-1, 1))
 
             # Add avg mut rate columns to splits to be saved
-            split_avg_mut = f"X_avg_mut_{bin_size}_{split_name}"
+            split_avg_mut = f"X_avg_mut_{label}_{split_name}"
             splitnames.append(split_avg_mut)
-            splits.append(dataset[f"avg_mut_{bin_size}"].to_numpy())
+            splits.append(dataset[f"avg_mut_{label}"].to_numpy().reshape(-1, 1))
+
+            # Add outcome-specific avg mut rate columns to splits to be saved (as one array)
+            split_avg_mut_per_type = f"X_avg_mut_{label}_pertype_{split_name}"
+            splitnames.append(split_avg_mut_per_type)
+            splits.append(dataset[[f"avg_mut_{label}_{base}" for base in ["A", "C", "G", "T"]]].to_numpy())
     
     filepath = "/faststorage/project/MutationAnalysis/Nimrod/data/splits"
     os.makedirs(filepath, exist_ok=True)
